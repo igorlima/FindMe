121632906f58a24b282f0ff82710c01bae1daff2
Igor Ribeiro Lima <lima.igorribeiro@gmail.com>
2012-11-11 13:10:57 -0200

    Adicionando libs para simular requisoes de json's

--- java/FindMe/project/Build.scala
+++ java/FindMe/project/Build.scala
@@ -8,11 +8,12 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      // Add your project dependencies here,
+      "mysql"                 % "mysql-connector-java" % "5.1.18",
+      "com.google.code.gson"  % "gson" % "2.2.2"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
-      // Add your own project settings here      
+      resolvers += "Maven repository" at "http://morphia.googlecode.com/svn/mavenrepo/"      
     )
 
 }
