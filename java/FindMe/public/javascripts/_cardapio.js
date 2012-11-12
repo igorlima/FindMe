ede8b8ea3adafb2aaffc00ae8ca6efd77cc76c11
Igor Ribeiro Lima <igor.lima@lemaf.ufla.br>
2012-11-12 01:52:39 -0200

    Criando estrutura de carregamento dinamico de js para cada pagina

---
+++ java/FindMe/public/javascripts/_cardapio.js
@@ -0,0 +1,7 @@
+;(function(window, $){
+  var Cardapio = window.Lanche.Cardapio = function(){};
+  Cardapio.load = function() {
+    //$('.teste').click(function(){console.warn('oi')});
+    //console.warn('oi');
+  };
+})(window, Zepto);
