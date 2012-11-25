589d4b673f11a6f9c7e642412d911534e356dfce
Igor Ribeiro Lima <lima.igorribeiro@gmail.com>
2012-11-25 14:21:16 -0200

    visibilidade do mapa

--- app/javascripts/_index.js
+++ app/javascripts/_index.js
@@ -41,10 +41,10 @@
 
     Path.map("#map").to(function(){
       head
-      .load("stylesheets/leaflet.min.css")
-      .js("javascripts/leaflet.min.js")
-      .js("javascripts/_map.js")
-      .js("javascripts/_local.js")
+      .load("stylesheets/leaflet.min.css", function(){ alert('css') } )
+      .js("javascripts/leaflet.min.js", function(){ alert('leaflet') } )
+      .js("javascripts/_map.js", function(){ alert('map') } )
+      .js("javascripts/_local.js", function(){ alert('local') } )
       .ready(function() {
         Lanche.Map.load();
       });
