ee97c6fa093dd3793c8ec660f7896af24c2be823
Igor Lima <igor.lima@evolut.io>
2012-12-05 22:33:50 -0200

    Armazenando imagem e primeiro nome

--- rubyonrails/app/controllers/sessions_controller.rb
+++ rubyonrails/app/controllers/sessions_controller.rb
@@ -1,7 +1,13 @@
 class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env["omniauth.auth"])
-    session[:user] = { name: user.name, provider: user.provider, uid: user.uid }
+    session[:user] = { 
+      provider: user.provider, 
+      uid: user.uid, 
+      name: user.name, 
+      firstName: user.first_name,
+      image: user.image 
+    }
     redirect_to '/'
   end
 
@@ -12,6 +18,7 @@ class SessionsController < ApplicationController
 
   def user
     respond_to do |format|
+      format.html { render json: session[:user] }
       format.json { render json: session[:user] }
     end
   end
--- rubyonrails/app/models/user.rb
+++ rubyonrails/app/models/user.rb
@@ -2,18 +2,21 @@ class User
   include Mongoid::Document
   field :provider, type: String
   field :uid, type: String
+  field :image, type: String
   field :name, type: String
+  field :first_name, type: String
   field :oauth_token, type: String
   field :oauth_expires_at, type: Time
 
   def self.from_omniauth(auth)
-
     user = User.where( uid: auth.uid, provider: auth.provider ).first
     user = User.new if user.nil?
 
     user.provider = auth.provider
     user.uid = auth.uid
     user.name = auth.info.name
+    user.first_name = auth.info.first_name
+    user.image = auth.info.image
     user.oauth_token = auth.credentials.token
     user.oauth_expires_at = Time.at(auth.credentials.expires_at)
     user.save
