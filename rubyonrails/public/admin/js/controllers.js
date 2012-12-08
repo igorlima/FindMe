a65dc6c86525167633a652cd5b9431e8c92e6f0a
Igor Lima <igor.lima@evolut.io>
2012-12-08 10:46:58 -0200

    Estruturando controllers

--- rubyonrails/public/admin/js/controllers.js
+++ rubyonrails/public/admin/js/controllers.js
@@ -8,47 +8,6 @@ lancheOnlineApp
     widthFunctions();
   }])
 
-  .controller( "CardapiosCtrl", ['$rootScope', '$scope', 'Cardapio', function(root, ng, Cardapio) {
-
-    root.cardapios = root.cardapios || [];
-
-    var listarCardapios = function() {
-      Cardapio.all(function(data){
-        root.cardapios = data;
-        ng.has_cardapios = ng.cardapios.length > 0 ? true : false;
-      });
-    };
-    listarCardapios();
-
-    ng.visualizar = function(cardapio) {
-      root.cardapio = cardapio;
-    };
-
-    ng.editarItens = function(cardapio) {
-      root.cardapio = cardapio;
-    };
-
-    ng.editar = function(cardapio) {
-      root.cardapio = cardapio;
-    };
-
-    ng.excluir = function(cardapio) {
-      root.cardapios = [];
-      listarCardapios();
-    };
-    
-    $('.btn-minimize').click(function(e){
-      e.preventDefault();
-      var $target = $(this).parent().parent().next('.box-content');
-      if($target.is(':visible')) $('i',$(this)).removeClass('icon-chevron-up').addClass('icon-chevron-down');
-      else             $('i',$(this)).removeClass('icon-chevron-down').addClass('icon-chevron-up');
-      $target.slideToggle();
-    });
-
-    activeCurrentLink();
-    widthFunctions();
-  }])
-
   .controller( "PedidosCtrl", ['$scope', function(ng) {
     activeCurrentLink();
     widthFunctions();
