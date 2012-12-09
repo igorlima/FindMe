b814d211389ea7be223568ba21a9604a5e5d10fe
Igor Lima <igor.lima@evolut.io>
2012-12-09 20:02:30 -0200

    Diminuindo limite

--- rubyonrails/app/controllers/messages_controller.rb
+++ rubyonrails/app/controllers/messages_controller.rb
@@ -2,7 +2,7 @@ class MessagesController < ApplicationController
   # GET /messages
   # GET /messages.json
   def index
-    @messages = Message.all.limit(35).asc( 'read' ).desc('updated_at')
+    @messages = Message.all.limit(25).asc( 'read' ).desc('updated_at')
 
     respond_to do |format|
       format.html # index.html.erb
