6d449cea05a1b57d6385e0a3ec9d5b013de92689
Igor Lima <igor.lima@evolut.io>
2012-12-09 18:25:29 -0200

    Utilizando remocao logica

--- rubyonrails/app/models/message.rb
+++ rubyonrails/app/models/message.rb
@@ -1,5 +1,8 @@
 class Message
   include Mongoid::Document
+  include Mongoid::Paranoia
+  include Mongoid::Timestamps
+
   field :name, type: String
   field :phone, type: String
   field :email, type: String
