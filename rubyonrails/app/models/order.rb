d1a4e23eb273b52fb60644fcb6718ece58ad2cce
Igor Lima <igor.lima@evolut.io>
2012-12-18 00:59:06 -0200

    refatoracao

--- rubyonrails/app/models/order.rb
+++ rubyonrails/app/models/order.rb
@@ -11,16 +11,24 @@ class Order
   belongs_to :user, polymorphic: true
   embeds_many :itens, :as => :order_itens, :class_name => "OrderItem"
 
-  def total
-    @total = 0.0;
+  def fee
+    @fee = 0.0
+    @configuration = StoreConfiguration.first_one
+    @fee += @configuration.online_fee
+    @fee += @configuration.delivery_fee if self.has_shipping
+    @fee
+  end
+
+  def total_itens
+    @total = 0.0
     self.itens.each do |item|
       @total += CardapioItem.find( item.cardapio_item_id ).price * item.qty
     end
-
-    @configuration = StoreConfiguration.first_one
-    @total += @configuration.online_fee
-    @total += @configuration.delivery_fee if self.has_shipping
     @total
+  end
+
+  def total
+    total_itens + fee
   end
 
 end
