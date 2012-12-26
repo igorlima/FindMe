68d018dc50ac9f2b90244e2e4c194a094ff6a7b9
Igor Lima <igor.lima@evolut.io>
2012-12-26 14:37:57 -0200

    Estrutura de pedidos no admin

--- rubyonrails/public/admin/js/controllers/pedidos.js
+++ rubyonrails/public/admin/js/controllers/pedidos.js
@@ -1,9 +1,55 @@
 'use strict';
 
 /* Controllers */
-lancheOnlineApp
-  .controller( "PedidosCtrl", ['$scope', function(ng) {
+lancheOnlineApp.
+
+controller( "PedidosCtrl", ['$rootScope', '$scope', 'Pedido',
+  function(root, ng, Pedido) {
+
+    var atualizarPedidos = function() {
+      !root.carregandoPedidos && root.listarPedidos();
+    };
+
+    ng.visualizar = function(pedido) {
+      ng.pedido = pedido;
+      $('#modalVisualizarPedido').modal('show');
+    };
+
+    ng.fazendoPedido = function() {
+      Pedido.doing( {id: ng.pedido.id}, {},
+        function(data){
+          atualizarPedidos();
+        },
+        function(data){
+          atualizarPedidos();
+        }
+      );
+    };
+
+    ng.pedidoPronto = function() {
+      Pedido.done( {id: ng.pedido.id}, {},
+        function(data){
+          atualizarPedidos();
+        },
+        function(data){
+          atualizarPedidos();
+        }
+      );
+    };
+
+    ng.pedidoEntregue = function() {
+      Pedido.delivered( {id: ng.pedido.id}, {},
+        function(data){
+          atualizarPedidos();
+        },
+        function(data){
+          atualizarPedidos();
+        }
+      );
+    };
+
     activeCurrentLink();
     widthFunctions();
-  }])
-  ;
+
+  }
+]);
