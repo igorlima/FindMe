787832b3b6cfde714e5345558250b82bca6142d1
Igor Lima <igor.lima@evolut.io>
2012-12-26 09:48:39 -0200

    Verificacao se o usuario estah logado sempre que acessar o home

--- rubyonrails/public/js/_user_data.js
+++ rubyonrails/public/js/_user_data.js
@@ -3,34 +3,42 @@
   var Lanche = window.Lanche = window.Lanche || function() {};
   var User = Lanche.User = Lanche.User || function() {};
 
-  $.getJSON('/user.json', function(data){
-    User.data = data;
-    if (data && data.uid) {
+  User.load = function() {
+    $.getJSON('/user.json', function(data){
+      User.data = data;
+      if (data && data.uid) authenticated(data);
+      else notAuthenticated(data);
+    });
+  };
 
-      $('#authentication a').attr('href', '/signout');
-      $('#authentication strong').html('Sair');
-      $('#bar-tab-item-user')
-      .empty()
-      .addClass('tab-item')
-      .append(""+
-        "<a href='#user'>"+
-          "<img class='tab-icon' src='img/icon-profile.png'>"+
-          "<div class='tab-label'>"+ data.firstName.substr(0,8) +"</div>"+
-        "</a>"
-      )
-      ;
+  var authenticated = function(data) {
+    var isAuthenticated = (data && data.uid);
+    if (!isAuthenticated) return;
 
-    } else {
+    $('#authentication a').attr('href', '/signout');
+    $('#authentication strong').html('Sair');
+    $('#bar-tab-item-user')
+    .empty()
+    .addClass('tab-item')
+    .append(""+
+      "<a href='#user'>"+
+        "<img class='tab-icon' src='img/icon-profile.png'>"+
+        "<div class='tab-label'>"+ data.firstName.substr(0,8) +"</div>"+
+      "</a>"
+    );
 
-      $('#authentication a').attr('href', '/auth/facebook');
-      $('#authentication strong').html('Logar usando Facebook');
-      $('#bar-tab-item-user')
-      .empty()
-      .removeClass('tab-item')
-      ;
-      
-    }
+  };
 
-  });
+  var notAuthenticated = function(data) {
+    var isAuthenticated = (data && data.uid);
+    if (isAuthenticated) return;
+
+    $('#authentication a').attr('href', '/auth/facebook');
+    $('#authentication strong').html('Logar usando Facebook');
+    $('#bar-tab-item-user')
+    .empty()
+    .removeClass('tab-item');
+
+  };
 
 })(window, Zepto);
