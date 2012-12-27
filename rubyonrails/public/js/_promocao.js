31882515e43e239ca07655d7c25c8321c667026e
Igor Lima <igor.lima@evolut.io>
2012-12-27 17:09:39 -0200

    refatoracao

--- rubyonrails/public/js/_promocao.js
+++ rubyonrails/public/js/_promocao.js
@@ -7,46 +7,12 @@
     viewModel.showBtnVoltar(true);
     viewModel.showMap(false);
 
-    createHtml();
-    applyBindings();
-    Lanche.spinner.stop();
-  };
-
-  var createHtml = function() {
-    $('#phone-app')
-    .empty()
-    .append(""+
-      "<div class='content-padded'>"+
-        "<p class='welcome'>Nós nos orgulhamos de poder lhe oferecer ótimos produtos com ótimos preços.</p>"+
-      "</div>"
-    )
-    .append(""+
-      "<div class='slider'>"+
-        "<ul>"+
-          "<li>"+
-            "<img src='img/promocao-1.jpg'>"+
-            "<span class='slide-text'><strong>← Arraste e se surpreenda</strong></span>"+
-          "</li>"+
-          "<li>"+
-            "<img src='img/promocao-2.jpg'>"+
-          "</li>"+
-          "<li>"+
-            "<img src='img/promocao-3.jpg'>"+
-          "</li>"+
-        "</ul>"+
-      "</div>"
-    )
-    .append(""+
-      "<ul class='list inset'>"+
-        "<li>X-Tudinho <a class='button-positive'>Comprar</a></li>"+
-        "<li>X-Picanha <a class='button-positive'>Comprar</a></li>"+
-        "<li>Lombo-Bacon-Egg <a class='button-positive'>Comprar</a></li>"+
-        "<li>Misto <a class='button-positive'>Comprar</a></li>"+
-      "</ul>"
-    );
-
-    Lanche.Slider.scan();
-    new Finger( $('.slider')[0] );
+    Lanche.spinner.start();
+    $('#phone-app').load( 'partials/promocao.html', function(){
+      applyBindings();
+      Lanche.spinner.stop();
+    });
+  
   };
 
   var applyBindings = function() {
