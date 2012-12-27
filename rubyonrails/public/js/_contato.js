47809e9250fd00f31fd1f60934ffa9a24890ec3e
Igor Lima <igor.lima@evolut.io>
2012-12-27 17:05:13 -0200

    refatoracao

--- rubyonrails/public/js/_contato.js
+++ rubyonrails/public/js/_contato.js
@@ -1,69 +1,39 @@
-;(function(Lanche, $, viewModel) {
+;(function(Lanche, $, vm) {
 
   var Contato = Lanche.Contato = function(){};
   Contato.load = function() {
-    viewModel.title('Contato');
-    viewModel.url_voltar('#home');
-    viewModel.showBtnVoltar(false);
-    viewModel.showMap(false);
+    vm.title('Contato');
+    vm.url_voltar('#home');
+    vm.showBtnVoltar(false);
+    vm.showMap(false);
 
-    createHtml();
-    applyBindings();
-    Lanche.spinner.stop();
+    $('#phone-app').load( 'partials/contato.html', function() {
+      applyBindings();
+      Lanche.spinner.stop();
+    });
   };
 
-  var createHtml = function() {
-    $('#phone-app')
-    .empty()
-    .append(""+
-      "<div class='content-padded'>"+
-        "<p class='welcome'>Estamos abertos a qualquer tipo de sugestão e críticas.</p>"+
-      "</div>"
-    )
-    .append(""+
-      "<form class='contato'>"+
-        "<div class='input-group'>"+
-          "<div class='input-row'>"+
-            "<label>Nome</label>"+
-            "<input type='text' placeholder='Igor Ribeiro Lima' data-bind='value: contato().name' >"+
-          "</div>"+
-          "<div class='input-row'>"+
-            "<label>Telefone</label>"+
-            "<input type='text' placeholder='(35) 9961-3395' data-bind='value: contato().phone' >"+
-          "</div>"+
-          "<div class='input-row'>"+
-            "<label>E-mail</label>"+
-            "<input type='email' placeholder='team@evolut.io' data-bind='value: contato().email' >"+
-          "</div>"+
-        "</div>"+
-        "<input type='search' placeholder='Assunto' data-bind='value: contato().subject' >"+
-        "<textarea rows='5' data-bind='value: contato().text' ></textarea>"+
-        "<a class='button button-block'>Enviar</a>"+
-      "</form>"
-    );
-
-    $('#phone-app form a.button').click(function() {
+  var applyBindings = function() {
+    vm.contato = ko.observable({
+      name: Lanche.User.data.name
+    });
+    vm.sendMessage = function() {
       Lanche.spinner.start();
       $.post('/messages.json', {
-          message: viewModel.contato(),
+          message: vm.contato(),
           authenticity_token: Lanche.User.data.authenticity_token
         },
         function(response) {
           response = JSON.parse(response);
           var message_errors = Lanche.Util.message_errors(response.errors);
           if (message_errors) alert(message_errors);
-          else viewModel.contato({});
+          else vm.contato({});
           Lanche.spinner.stop();
         }
       );
-    });
-  };
+    };
 
-  var applyBindings = function() {
-    viewModel.contato = ko.observable({
-      name: Lanche.User.data.name
-    });
-    ko.applyBindings(viewModel);
+    ko.applyBindings(vm);
   };
   
 })(window.Lanche, Zepto, Lanche.viewModel);
