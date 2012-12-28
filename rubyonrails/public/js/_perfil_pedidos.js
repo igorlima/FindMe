6d2e4d72ba710eb579db8ab3dcfe24effc3408c3
Igor Lima <igor.lima@evolut.io>
2012-12-28 13:41:47 -0200

    Adicionando menu de pedidos no perfil do usuario

--- rubyonrails/public/js/_perfil_pedidos.js
+++ rubyonrails/public/js/_perfil_pedidos.js
@@ -1,4 +1,4 @@
-;(function(Perfil, $, vm) {
+;(function(Perfil, $, ko, vm) {
 
   var Ordens = Perfil.Ordens = Lanche.Ordens || function() {};
   Ordens.load = function() {
@@ -7,16 +7,25 @@
     vm.showBtnVoltar(true);
     vm.showMap(false);
 
-    Lanche.spinner.start();
-    $('#phone-app').load( 'partials/perfil_ordens.html', function(){
-      applyBindings();
-      Lanche.spinner.stop();
+    $.getJSON('/user/lastOrders.json', function(data) {
+      vm.pedidos(data);
+      $('#phone-app').load( 'partials/perfil_pedidos.html', function(){
+        applyBindings();
+        Lanche.spinner.stop();
+      });
     });
 
   };
 
   var applyBindings = function() {
+    var hasPedidos = vm.pedidos() ? vm.pedidos().length > 0 : false;
+    vm.hasPedidos = ko.observable( hasPedidos );
     ko.applyBindings(vm);
   };
+
+  // Observable
+  !function () {
+    vm.pedidos = ko.observable(null);
+  }();
   
-})(window.Lanche.Perfil, Zepto, Lanche.viewModel);
+})(window.Lanche.Perfil, Zepto, ko, Lanche.viewModel);
