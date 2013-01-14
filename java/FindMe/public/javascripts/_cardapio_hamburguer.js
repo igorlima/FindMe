6743c35b4020e31244dc94bee7969cf4e6a63cc1
Igor Lima <igor.lima@evolut.io>
2013-01-14 21:04:29 -0200

    Remover arquivos desnecessarios

--- java/FindMe/public/javascripts/_cardapio_hamburguer.js
+++
@@ -1,8 +0,0 @@
-;(function(window, $){
-  var Hamburguer = window.Lanche.Cardapio.Hamburguer = function(){};
-  Hamburguer.load = function() {
-    $('.button-positive').click(function(){
-      alert('Compra efetuada com sucesso.');
-    });
-  };
-})(window, Zepto);
